6dbb988392910e3f836a8fd21c62e8ab88daebca
SuperCrunch <jesus.ramirez@bba06.mccombs.utexas.edu>
2013-10-31 13:52:44 -0500

    adding gunicorn to installed apps

--- mezzanine/project_template/settings.py
+++ mezzanine/project_template/settings.py
@@ -242,6 +242,7 @@ INSTALLED_APPS = (
     "mezzanine.twitter",
     #"mezzanine.accounts",
     #"mezzanine.mobile",
+    "gunicorn",
 )
 
 # List of processors used by RequestContext to populate the context.
